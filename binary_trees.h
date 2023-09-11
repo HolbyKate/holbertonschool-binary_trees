ec7e199fdb5ce8448374b3aac6824a5b4b3bd6c1
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2023-09-11 13:32:14 +0000

    fix: header

--- binary_trees.h
+++ binary_trees.h
@@ -2,8 +2,6 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,16 +11,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
-
-typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
