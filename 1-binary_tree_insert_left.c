a53293d4aca695a76838b6d0a2b63915827d2ff6
root <you@example.com>
2023-09-12 07:13:55 -0700

    fix betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
+
 	new_node = binary_tree_node(parent, value);
 
 	if (parent == NULL)
@@ -27,5 +28,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
-	return(new_node);
+	return (new_node);
 }
