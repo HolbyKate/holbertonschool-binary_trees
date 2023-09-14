f36f46d3046ab4d2f09a4a962421534c47e8a0f7
root <you@example.com>
2023-09-14 14:21:08 -0700

    Fix : value of return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,10 +16,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	else
-	{		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
-	return (-1);
+	return (0);
 }
